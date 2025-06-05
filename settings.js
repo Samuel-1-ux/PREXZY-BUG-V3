16b2910c2315c4bd0125713a429d02cb159f936b
Samuel-1-ux <michelkouadja000@gmail.com>
2025-06-05 14:41:49 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
 global.ownernumber = '2250712228448'  //creator number
-global.ownername = '•HᴇᴀʀᴛᵇʳᵉᵃKIᑎᘜ✞🦁' //owner name
+global.ownername = '•ynn🦁' //owner name
 global.botname = '𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻3' //name of the bot
 
 //sticker details
